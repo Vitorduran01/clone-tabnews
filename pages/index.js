60a94e6550d18129f0b65ab9b14a6bfe2d19195b
Vitorduran01 <vitorduran88@gmail.com>
2025-12-04 17:09:40 -0300

    mais testes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>"Você é o amor da minha vida e a razão
+    return <h1>"Você é o amor da minha vida e a razão, mais e mais testes
         dos meus sorrisos mais sinceros. Eu te amo meu amor, teste"
     </h1>
 }
